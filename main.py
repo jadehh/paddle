5c3cb7b398b3099d952c1ecbaa3b9cfc0f805fc6
jade <jadehh@live.com>
2021-05-06 10:09:11 +0800

    update 1.更新paddle导入库

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File     : main.py
+# @Author   : jade
+# @Date     : 2021/5/6 10:05
+# @Email    : jadehh@1ive.com
+# @Software : Samples
+# @Desc     :
+from paddle.fluid.core import AnalysisConfig
