79aa391b27663d6e971a66f2827cefcc0ff9a87c
jade <jadehh@live.com>
2022-12-06 16:12:23 +0800

    update v1.7.2-gpu-linux

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 # @Email    : jadehh@1ive.com
 # @Software : Samples
 # @Desc     :
+import sys
+sys.path.append("/usr/local/python_lib")
 from paddle.fluid.core import AnalysisConfig
 from paddle.fluid.core import create_paddle_predictor
 import paddle
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="paddle",
-    version="1.8",
+    version="1.7.2",
     keywords=("pip", "paddle", ""),
     description="paddle",
     long_description="xxx",
